d078f782fb1cf3468dbb5daea964ad55b3f51c76
mrblueblue <grokut@gmail.com>
2015-02-24 11:51:38 -0800

    refactored using express

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,41 +1,63 @@
-/* Import node's http module: */
-var http = require("http");
+var fs = require('fs');
+var fake;
+fs.readFile('./messages.txt', function(err, data){
+  fake = JSON.parse(data.toString());
+});
 
 
-// Every server needs to listen on a port with a unique number. The
-// standard port for HTTP servers is port 80, but that port is
-// normally already claimed by another server and/or not accessible
-// so we'll use a standard testing port like 3000, other common development
-// ports are 8080 and 1337.
+var express = require('express');
 var port = 3000;
-
-// For now, since you're running this server on your local machine,
-// we'll have it listen on the IP address 127.0.0.1, which is a
-// special address that always refers to localhost.
 var ip = "127.0.0.1";
 
+var app = express();
+
+app.use(express.static('../client'));
+
+app.get('/', function(req, res){
+  res.status(200).sendfile('index.html', {root: 'client'});
+});
+
+app.get('/classes', function(req, res){
+  res.status(200).json(fake);
+});
+
+app.post('/classes', function(req, res){
+  req.on('data', function(data){
+    var parsed = JSON.parse(data.toString());
+    fake.results.unshift(parsed);
+    fs.writeFile('./messages.txt', JSON.stringify(fake));
+    res.status(201).send();
+  });
+});
 
 
-// We use node's http module to create a server.
-//
-// The function we pass to http.createServer will be used to handle all
-// incoming requests.
-//
-// After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
-console.log("Listening on http://" + ip + ":" + port);
-server.listen(port, ip);
 
-// To start this server, run:
-//
-//   node basic-server.js
-//
-// on the command line.
-//
-// To connect to the server, load http://127.0.0.1:3000 in your web
-// browser.
-//
-// server.listen() will continue running as long as there is the
-// possibility of serving more requests. To stop your server, hit
-// Ctrl-C on the command line.
+var server = app.listen(port, function(){
+  var host = server.address().address;
+  var daport = server.address().port;
+
+  console.log(host + ': ' + daport);
+});
+
+
+var defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
