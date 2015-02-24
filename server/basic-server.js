1f79c824dc80fbffe2479490177f4ca02707f763
mrblueblue <grokut@gmail.com>
2015-02-24 12:18:54 -0800

    code clean

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,13 +1,12 @@
+
 var fs = require('fs');
+var express = require('express');
+var port = 3000;
+
 var fake;
 fs.readFile('./messages.txt', function(err, data){
   fake = JSON.parse(data.toString());
 });
-
-
-var express = require('express');
-var port = 3000;
-var ip = "127.0.0.1";
 
 var app = express();
 
@@ -30,22 +29,12 @@ app.post('/classes', function(req, res){
   });
 });
 
-
-
 var server = app.listen(port, function(){
   var host = server.address().address;
   var daport = server.address().port;
 
   console.log(host + ': ' + daport);
 });
-
-
-var defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
-};
 
 
 
