aa8fe43c45ef1d9f07707386020fe25b0bdb8973
Jonathan Huang <grokut@gmail.com>
2015-02-24 18:39:58 -0800

    added cors headers

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,43 +1,48 @@
 
 var fs = require('fs');
 var express = require('express');
+var cors = require('cors')
+var app = express();
 var port = 3000;
+var database;
 
-var fake;
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
+};
+
 fs.readFile('./messages.txt', function(err, data){
-  fake = JSON.parse(data.toString());
+  database = JSON.parse(data.toString());
 });
 
-var app = express();
-
 app.use(express.static('../client'));
+app.use(cors());
 
 app.get('/', function(req, res){
-  res.status(200).sendfile('index.html', {root: 'client'});
+  res.header(headers).status(200).sendfile('index.html', {root: 'client'});
 });
 
 app.get('/classes', function(req, res){
-  res.status(200).json(fake);
+  res.header(headers).status(200).json(database);
 });
 
 app.post('/classes', function(req, res){
   req.on('data', function(data){
     var parsed = JSON.parse(data.toString());
-    fake.results.unshift(parsed);
-    fs.writeFile('./messages.txt', JSON.stringify(fake));
-    res.status(201).send();
+    database.results.unshift(parsed);
+    fs.writeFile('./messages.txt', JSON.stringify(database));
+    res.header(headers).status(201).send();
   });
 });
 
 var server = app.listen(port, function(){
   var host = server.address().address;
-  var daport = server.address().port;
+  var port = server.address().port;
 
-  console.log(host + ': ' + daport);
+  console.log('listening @ ' + host + ': ' + port);
 });
-
-
-
 
 
 
